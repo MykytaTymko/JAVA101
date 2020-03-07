66a776c8b102852fedcae5eb6cb89c72db95110c
mykyta_tymko <57773598+nik00768@users.noreply.github.com>
2020-03-07 06:35:05 +0600

    Add files via upload

---
+++ Objective_3.java
@@ -0,0 +1,32 @@
+package Objectives;
+
+public class Objective_3 
+{
+    	public static void main(String[] args)
+        {
+		int[] array = {30, 2, 10, 4, 6};
+		int length = array.length;
+                boolean isSorted = false;
+                int temp;
+                
+                while(!isSorted)
+                {
+                    isSorted = true;
+                    
+                    for ( int j = 0; j < array.length - 1; j++)
+                    {
+                        if(array[j] > array[j+1])
+                        {
+                            isSorted = false;
+                            temp = array[j];
+                            array[j] = array[j+1];
+                            array[j+1] = temp;
+                        }
+                    }
+                }
+
+		for (int i = 0; i < length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
